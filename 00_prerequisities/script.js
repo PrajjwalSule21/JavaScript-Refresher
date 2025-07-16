febf7073e2f0eba8cbdb2dbb5f6ac3e3b8c011ab
PrajjwalSule21 <suleprajjwal21@gmail.com>
2025-07-16 21:18:26 +0530

    prerequisities

---
+++ 00_prerequisities/script.js
@@ -0,0 +1,14 @@
+// words and keywords
+
+prajjwal // word
+var // keyword
+sule // word
+lucky // word
+
+let // keyword
+if // keyword
+const // keyword
+
+
+// if a word has a meaning or have some operation in js is keyword
+// a normal word which don't have any pre operation/function in JS that can simply define as word
